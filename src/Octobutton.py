4d9d5e312cd7fd512d1b54cec5abb9d544fce4d3
Isaac Poole <isaac@switcherstudio.com>
2023-09-17 11:49:13 -0400

    adds buttons

--- src/Octobutton.py
+++ src/Octobutton.py
@@ -1,14 +1,21 @@
 from __future__ import with_statement
+from typing import List
 
 import Live
+from _Framework.ButtonElement import ButtonElement
 
 from _Framework.ControlSurface import ControlSurface
+from _Framework.InputControlElement import MIDI_NOTE_TYPE
 from .RecordingSessionComponent import RecordingSessionComponent
+from .mappings import CHANNEL, PRESSED_NOTES, HELD_NOTES
 
 
 class Octobutton(ControlSurface):
     __module__ = __name__
     __doc__ = "SCript for the Octobutton MIDI device"
+    recording_session: RecordingSessionComponent
+    pressed_buttons: List[ButtonElement]
+    held_buttons: List[ButtonElement]
 
     def __init__(self, c_instance):
         ControlSurface.__init__(self, c_instance)
@@ -26,5 +33,7 @@ class Octobutton(ControlSurface):
         return None
 
     def _setup_recording_session(self):
-        self.recording_session = RecordingSessionComponent()
+        self.recording_session = RecordingSessionComponent(
+            PRESSED_NOTES, HELD_NOTES, self.log_message
+        )
         self.set_highlighting_session_component(self.recording_session)
--- src/RecordingSessionComponent.py
+++ src/RecordingSessionComponent.py
@@ -1,8 +1,35 @@
+from typing import List
+from _Framework.ButtonElement import ButtonElement
+from _Framework.InputControlElement import MIDI_NOTE_TYPE
 from _Framework.SessionComponent import SessionComponent
+from .mappings import CHANNEL
 
 
 class RecordingSessionComponent(SessionComponent):
-    def __init__(self):
+    def __init__(self, pressed_notes: List[int], held_notes: List[int], log=None):
         SessionComponent.__init__(
             self, num_tracks=8, num_scenes=1, auto_name=False, enable_skinning=False
         )
+
+        self.log = log
+        self._setup_buttons(pressed_notes, held_notes)
+
+    def _setup_buttons(self, pressed_notes: List[int], held_notes: List[int]):
+        self.pressed_buttons = []
+        self.held_buttons = []
+
+        for note in pressed_notes:
+            button = ButtonElement(True, MIDI_NOTE_TYPE, CHANNEL, identifier=note)
+            button.add_value_listener(self.pressed_value_listener)
+            self.pressed_buttons.append(button)
+
+        for note in held_notes:
+            button = ButtonElement(True, MIDI_NOTE_TYPE, CHANNEL, identifier=note)
+            button.add_value_listener(self.held_value_listener)
+            self.held_buttons.append(button)
+
+    def pressed_value_listener(self, value: int):
+        self.log(value)
+
+    def held_value_listener(self, value: int):
+        self.log(value)
