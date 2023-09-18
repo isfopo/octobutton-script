aa8fa2d390be634fbbcc329efda0e348415581b5
Isaac Poole <isaac@switcherstudio.com>
2023-09-18 12:35:48 -0400

    adds docs

--- src/helpers/track.py
+++ src/helpers/track.py
@@ -2,6 +2,14 @@ from Live import Clip, ClipSlot, Track
 
 
 def get_last_clip(track: Track.Track) -> Clip.Clip:
+    """Gets the bottommost Clip in the Track, given that there are no empty Clip Slots. If so, this function will return the Clip before the empty Slot.
+
+    Args:
+        track (Track.Track): An Ableton Live Track
+
+    Returns:
+        Clip.Clip: Returns None if all Slots are empty
+    """
     slot: ClipSlot.ClipSlot
     clip: Clip.Clip = None
     for slot in track.clip_slots:
